f9c4ba05d1a79f6e91be4149f8b4e83e45d6368f
Sumon Chandra <topu199041@gmail.com>
2023-06-25 21:13:07 +0600

    Add react tilt on skill card

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,21 +8,33 @@ import mongodb from "../assets/icons/mongodb.png";
 import node from "../assets/icons/node-js.png";
 import firebase from "../assets/icons/firebase.png";
 import express from "../assets/icons/express.png";
+import Tilt from "react-parallax-tilt";
 
 interface SkillTypes {
   skill: string;
   img: string;
+  index: number;
 }
 
-const SkillCard = ({ skill, img }: SkillTypes) => {
+const SkillCard = ({ skill, img, index }: SkillTypes) => {
   return (
-    <div className="bg-[#162033] p-6 flex flex-col items-center gap-3 border-b-2 border-main-color shadow-sm shadow-main-color duration-500 hover:-translate-y-2">
-      <img
-        src={img}
-        className="lg:w-16 w-10 duration-500 saturate-0 hover:saturate-100"
-      />
-      <h3 className="text-desc-color lg:text-2xl font-black">{skill}</h3>
-    </div>
+    <Tilt
+      className={`card bg-[#162033] rounded-3xl shadow-2xl mx-auto w-40 h-40 select-none ${
+        index % 2 === 0 ? "lg:mt-0 mt-5" : "lg:mt-28 mt-5"
+      }`}
+      perspective={500}
+      scale={1.1}
+    >
+      <div>
+        <img
+          src={img}
+          className="object-contain mx-auto p-4 h-32 duration-500 saturate-0 hover:saturate-100"
+        />
+        <h3 className="text-desc-color lg:text-lg font-black text-center">
+          {skill}
+        </h3>
+      </div>
+    </Tilt>
   );
 };
 
@@ -32,15 +44,15 @@ const Skills = () => {
       <SectionHead parallaxValue="skills" value="my skills" />
 
       <div className=" grid grid-cols-2 lg:grid-cols-3 gap-7 lg:pt-20">
-        <SkillCard skill="TypeScript" img={ts} />
-        <SkillCard skill="JavaScript" img={js} />
-        <SkillCard skill="ReactJS" img={react} />
-        <SkillCard skill="ReduxJS" img={redux} />
-        <SkillCard skill="NodeJS" img={node} />
-        <SkillCard skill="ExpressJS" img={express} />
-        <SkillCard skill="MongoDB" img={mongodb} />
-        <SkillCard skill="Firebase" img={firebase} />
-        <SkillCard skill="Figma" img={figma} />
+        <SkillCard index={1} skill="TypeScript" img={ts} />
+        <SkillCard index={2} skill="JavaScript" img={js} />
+        <SkillCard index={3} skill="ReactJS" img={react} />
+        <SkillCard index={4} skill="ReduxJS" img={redux} />
+        <SkillCard index={5} skill="NodeJS" img={node} />
+        <SkillCard index={6} skill="ExpressJS" img={express} />
+        <SkillCard index={7} skill="MongoDB" img={mongodb} />
+        <SkillCard index={8} skill="Firebase" img={firebase} />
+        <SkillCard index={9} skill="Figma" img={figma} />
       </div>
     </section>
   );
